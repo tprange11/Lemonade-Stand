2a74b6101135fb2d4075f66af1f86a1b97f7e203
tprange11 <tprange@gmail.com>
2018-03-29 09:51:58 -0500

    Adding SQL Connection

---
+++ Lemonade-Stand/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+
+namespace Lemonade_Stand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string connectionString = "SERVER = localhost, DATABASE = SBM";
+            SqlConnection sqlconn = new SqlConnection(connectionString);
+            sqlconn.Open();
+            
+            
+        }
+    }
+}
