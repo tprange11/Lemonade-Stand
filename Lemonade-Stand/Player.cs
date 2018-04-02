a04281a425f160dc8774b2c72ab80b6f575f29e1
tprange11 <tprange@gmail.com>
2018-04-02 14:40:52 -0500

    Added wallet, city and state variables and incorporated using the weather class.

--- Lemonade-Stand/Player.cs
+++ Lemonade-Stand/Player.cs
@@ -6,15 +6,37 @@ namespace LemonadeStand
 {
     class Player
     {
-    // member variables
-    public string name;
+        // member variables
+        string todd;
+        public string name;
+        public double wallet;
+        public string city;
+        public string state;
 
-    // constructor
-    public Player()
-    {
+        // constructor
+        public Player()
+        {
+            wallet = 5.00;
+           
+        }
+        // member methods
+        public virtual string DisplayPlayerName()
+        {
+            return name;
+        }
 
-    }
-    // member methods
+        public virtual void SetPlayerName()
+        {
+            User_Interface.DisplayMessage("What is your name?\n");
+            name = User_Interface.GetUserInput();
+        }
+        public void GetLocation()
+        {
+            User_Interface.DisplayMessage("\nWhat city is your Lemonade Stand in?");
+            city = User_Interface.GetUserInput();
+            User_Interface.DisplayMessage("\nWhat state is your Lemonade Stand in?");
+            state = User_Interface.GetUserInput();
+        }
 
     }
 }
