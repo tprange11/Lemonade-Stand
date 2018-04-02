29baca52bf55ae7ecc9e294cb96648057e6fc527
tprange11 <tprange@gmail.com>
2018-04-02 14:41:34 -0500

    Added the Computer Class.

---
+++ Lemonade-Stand/Computer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LemonadeStand
+{
+    class Computer : Player
+    {
+        // member variables
+
+
+        // constructor
+        public Computer()
+        {
+            SetPlayerName();
+        }
+        // member methods
+        public override void SetPlayerName()
+        {
+            name = "Sophia";
+        }
+
+    }
+}
