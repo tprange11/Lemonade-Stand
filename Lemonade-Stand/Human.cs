a37d44a444cc84d68535c90ce97ded64d1577215
tprange11 <tprange@gmail.com>
2018-04-02 14:41:11 -0500

    Added the Human class

---
+++ Lemonade-Stand/Human.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LemonadeStand
+{
+    class Human : Player
+    {
+        // member variables
+
+        // constructor
+        public Human()
+        {
+            SetPlayerName();
+
+        }
+        // member methods
+        public override void SetPlayerName()
+        {
+            User_Interface.DisplayMessage("What is your name?\n");
+            name = User_Interface.GetUserInput();
+        }
+        public static string SelectOpponent()
+        {
+            string response;
+            User_Interface.DisplayMessage("Would you like to play the computer?");
+            response = User_Interface.GetUserInput();
+            return response;
+
+        }
+
+    }
+}
