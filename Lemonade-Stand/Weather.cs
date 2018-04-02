1463133ca7ead87d9ba934b4bc1318f04ab9f67d
tprange11 <tprange@gmail.com>
2018-04-02 14:38:47 -0500

    Updated weather class to use the weatherUnderground API.

--- Lemonade-Stand/Weather.cs
+++ Lemonade-Stand/Weather.cs
@@ -3,28 +3,60 @@ using System.IO;
 using System.Collections.Generic;
 using System.Net;
 using System.Text;
+using System.Xml;
 
 namespace LemonadeStand
 {
     public static class Weather
     {
-        public static void GetWeather()
+        public static void GetWeather(string date,string city, string state)
         {
-            WebRequest request = WebRequest.Create("http://api.openweathermap.org/data/2.5/forecast?q=Milwaukee&appid=97e75e23779deb19bb9b1ecc5b58f445");
-            WebResponse response = request.GetResponse();
-            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-            // Get the stream containing content returned by the server.  
-            Stream dataStream = response.GetResponseStream();
-            // Open the stream using a StreamReader for easy access.  
-            StreamReader reader = new StreamReader(dataStream);
-            // Read the content.  
-            string responseFromServer = reader.ReadToEnd();
-            // Display the content.  
-            User_Interface.DisplayMessage(responseFromServer);
-            //dynamic stuff = JsonConvert.DeserializeObject(responseFromServer);
-            // Clean up the streams and the response.  
-            reader.Close();
-            response.Close();
+            //Start program
+            Console.WriteLine("Starting C# Weather Undeground Web API Test...");
+            string wunderground_key = "df1e650266f41157"; // You'll need to goto http://www.wunderground.com/weather/api/, and get a key to use the API.
+
+            parse("http://api.wunderground.com/api/" + wunderground_key + "/history_" + date + "/q/" + state + "/" + city + ".xml");
+
+            // End.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+        private static void parse(string input_xml)
+        {
+            //Variables
+            string maxtempi = "";
+            string conds = "";
+
+            var cli = new WebClient();
+            string weather = cli.DownloadString(input_xml);
+
+            using (XmlReader reader = XmlReader.Create(new StringReader(weather)))
+            {
+                // Parse the file and display each of the nodes.
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            if (reader.Name.Equals("maxtempi"))
+                            {
+                                reader.Read();
+                                maxtempi = reader.Value;
+                            }
+                            else if (reader.Name.Equals("conds"))
+                            {
+                                reader.Read();
+                                conds = reader.Value;
+                            }
+
+                            break;
+                    }
+                }
+            }
+
+            Console.WriteLine("********************");
+            Console.WriteLine("High Temp:           " + maxtempi);
+            Console.WriteLine("Conditions:       " + conds);
         }
 
     }
