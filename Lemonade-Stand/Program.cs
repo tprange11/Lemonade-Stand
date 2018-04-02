0af5ed085850a2c67bcafbd6fe9f987b57a850f4
tprange11 <tprange@gmail.com>
2018-04-02 14:43:22 -0500

    incorporated the using the weather class.

--- Lemonade-Stand/Program.cs
+++ Lemonade-Stand/Program.cs
@@ -8,16 +8,47 @@ using System.Data.SqlClient;
 
 
 
+
 namespace LemonadeStand
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+            Console.WriteLine("Introduciton:\n");
             Game game = new Game();
             game.Introduction();
             game.GetGameDuration();
+            Player player1 = new Human();
+            Player player2 = null;
+            switch (Human.SelectOpponent().ToLower())
+            {
+                case "yes":
+                    player2 = new Computer();
+                    break;
+                case "no":
+                    player2 = new Human();
+                    break;
+                default:
+                    break;
+            }
+            User_Interface.DisplayMessage(message: "Player1: " + player1.DisplayPlayerName() + "\tPlayer2: " + player2.DisplayPlayerName());  //Remove this line when done.
+            int i = 0;
+            while (game.daysPlayed > 0)
+            {
+                i++;
+                User_Interface.DisplayMessage("day: " + i);
+
+                DateTime dt = DateTime.Now.AddDays(-game.daysPlayed);
+                String strDate = "";
+                strDate = dt.ToString("yyyyMMdd");
+                User_Interface.DisplayMessage("\t Weather date is: " + strDate);
+                Weather.GetWeather(strDate, "Germantown", "WI");
+                game.daysPlayed -= 1;
+            }
+  
 
             string connectionString = "SERVER = desktop-ca2gle0; DATABASE = LemonadeStand; Integrated Security=true;";
             SqlConnection sqlconn = new SqlConnection(connectionString);
@@ -25,7 +56,7 @@ namespace LemonadeStand
             HighScore_tb.InsertPlayerScore("Todd", "2.67", sqlconn);
             HighScore_tb.GetTopTenScores(sqlconn);
             sqlconn.Close();
-            Weather.GetWeather();
+            //Weather.GetWeather();
             User_Interface.GetUserInput();
 
             
