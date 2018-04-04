3032f8f2d0fbbfabfe6d25034a46173d1c0aa1bb
tprange11 <tprange@gmail.com>
2018-04-04 15:43:35 -0500

    Added SQL class

---
+++ Lemonade-Stand/SQL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Data.SqlClient;
+using System.Text;
+
+
+namespace LemonadeStand
+{
+    static class SQL
+    {
+     public static List<T> DataReaderMapToList<T>(IDataReader dr)
+    {
+        List<T> list = new List<T>();
+        T obj = default(T);
+        while (dr.Read())
+        {
+            obj = Activator.CreateInstance<T>();
+            foreach (PropertyInfo prop in obj.GetType().GetProperties())
+            {
+                if (!object.Equals(dr[prop.Name], DBNull.Value))
+                {
+                    prop.SetValue(obj, dr[prop.Name], null);
+                }
+            }
+            list.Add(obj);
+        }
+        return list;
+    }
+
+}
+}
